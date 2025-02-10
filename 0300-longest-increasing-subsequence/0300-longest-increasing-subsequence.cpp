3e711920a599c89e91b109588339234ab02b9579
Vijeta Priya <67923889+VijetaPriya47@users.noreply.github.com>
2025-02-10 23:02:40 +0530

    Time: 559 ms (16.29%), Space: 297.4 MB (6.79%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,31 +1,32 @@
+
 class Solution {
 public:
-    int rec(vector<int>nums, int last,int ind,vector<vector<int>> dp){
-        if(ind==nums.size()){
-            return 0;
+    int  rec(int lvl, int prev, vector<int>&arr, vector<vector<int>>& dp){
+        if (lvl == arr.size()) {
+            return 0; // At the end, the LIS length is 0
         }
-
         
-        if( dp[ind][last+1]!=-1){
-            return dp[ind][last+1];
+        if (dp[lvl][prev+1] != -1) {
+            return dp[lvl][prev+1]; // Return the stored result
         }
 
-        //Take
-        int take=0;
-        if(last==-1 || nums[ind]>nums[last])
-            take=1+rec(nums,ind,ind+1,dp);
+        // Case 1: Don't take the current element
+        int notTake = rec(lvl + 1, prev, arr, dp);
 
-        //Nottake
-        int nttake=rec(nums,last,ind+1,dp);
+        // Case 2: Take the current element (only if it forms an increasing subsequence)
+        int take = 0;
+        if (prev == -1 || arr[lvl] > arr[prev]) {
+            take = 1 + rec(lvl + 1, lvl, arr, dp); // Increment length by 1 when taking the element
+        }
 
-        return dp[ind][last+1]=max(take,nttake);
+        // Store the result in dp and return the max of taking or not taking the element
+        return dp[lvl][prev+1] = max(take, notTake);
     }
-    int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
-        // nums.insert(nums.begin(),-10001);
-        vector<vector<int>>dp(n+2,vector<int>(n+2,-1));
-        // dp[0][0]=0;
-        return rec(nums,-1,0,dp);
-        
+    int lengthOfLIS(vector<int>& n) {
+        if(n.size()==1)return 1;  
+        int sz=n.size();
+        vector<vector<int>>dp(sz+6,vector<int>(sz+6,-1));
+
+        return rec(0,-1,n,dp);
     }
 };
