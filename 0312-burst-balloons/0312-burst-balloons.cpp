0f2d3af63c529045c42a4150fc66f056bc0a6fd2
Vijeta Priya <67923889+VijetaPriya47@users.noreply.github.com>
2025-02-10 19:11:16 +0530

    Time: 83 ms (96.26%), Space: 13.8 MB (56.8%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,56 +1,40 @@
 class Solution {
 public:
-    int n;
-    int t[501][501]; // For memoization
-    int solve(vector<int> &nums, int i, int j){
-		// BASE CASES
-		if(i > j)
-			return 0;
-        if(i == j){    // Only one element exists
-            int temp = nums[i];
-            if(i - 1 >= 0)  
-                temp *= nums[i - 1];
-            if(i + 1 < n)
-                temp *= nums[i + 1];
-            return temp;
-        }
-		if(t[i][j] != -1)  // Check if the solution is already stored for this subproblem
-			return t[i][j];
-        int ans = 0;
-		
-		// For all elements in the range i to j, we choose all of them one by one 
-		// to make them the last balloon to be burst. 
-        for(int k = i; k <= j; k++){
-		
-		    // Burst the kth balloon after bursting (i, k - 1) and (k + 1, j) balloons
-            int temp = nums[k];
-			
-            if(j + 1 < n)  // As balloon j + 1 will become adjacent to k after bursting  k + 1 to j balloons
-                temp *= nums[j + 1];
-				
-            if(i - 1 >= 0) // As balloon i- 1 will become adjacent to k after bursting  i  to k -1 balloons
-                temp *= nums[i - 1];
-				
-			// Recursively solve the left and right subproblems and add their contribution
-            temp += (solve(nums, i, k - 1) + solve(nums, k + 1, j));
-			
-			// If this choice of k yields a better answer
-            ans = max(ans, temp);
-        }
-        return t[i][j] = ans;
-    }
-    
+
+//For recursion: 
+// We are using a bottom top approach, and going such that what if Kth baloon has been burst last.
+// Exponential time complexity
+
+//For Memoization:
+// Use DP to store ans to dp[i][k-1] in recursion if has done before, then take it's value;
+
+//For Tabulation:
     int maxCoins(vector<int>& nums) {
-        memset(t, -1, sizeof(t));
-		
-        // Insert two dummy balloons of value 1 to handle the balloons on the corner.
-		vector<int> arr = {1};
-        for(int x: nums) 
-			arr.push_back(x);
-        arr.push_back(1);
-        n = arr.size();
-		
-		//Start from i = 1 and j = arr.size() - 2 since first and last balloons are dummy.
-        return solve(arr, 1, arr.size() - 2);
+            int n = nums.size();
+            
+            nums.insert(nums.begin(), 1);
+            nums.push_back(1);
+            
+            vector<vector<int>> dp(n + 2, vector<int>(n + 2, 0));
+
+            for (int i = n; i >= 1; i--) {
+                for (int j = 1; j <= n; j++) {
+                    if (i > j) continue;
+                    int maxi = INT_MIN;
+                    
+
+                    for (int ind = i; ind <= j; ind++) {
+                        int coins = nums[i - 1] * nums[ind] * nums[j + 1];
+
+                        int remainingCoins = dp[i][ind - 1] + dp[ind + 1][j];
+                        
+                        maxi = max(maxi, coins + remainingCoins);
+                    }
+                    
+                    dp[i][j] = maxi;
+                }
+            }
+            
+            return dp[1][n];
     }
 };
