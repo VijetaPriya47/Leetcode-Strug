38cd55c20e7298644455613f103e54fff6a95284
Vijeta Priya <67923889+VijetaPriya47@users.noreply.github.com>
2025-02-10 23:07:30 +0530

    Time: 607 ms (6.97%), Space: 296.8 MB (6.79%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    int  rec(int lvl, int prev, vector<int>&arr, vector<vector<int>>& dp){
-        if (lvl == arr.size()) {
-            return 0; // At the end, the LIS length is 0
+    int rec( int ind,int last,vector<int> &nums,vector<vector<int>> &dp){
+        if(ind==nums.size()){
+            return 0;
         }
+
         
-        if (dp[lvl][prev+1] != -1) {
-            return dp[lvl][prev+1]; // Return the stored result
+        if( dp[ind][last+1]!=-1){
+            return dp[ind][last+1];
         }
 
-        // Case 1: Don't take the current element
-        int notTake = rec(lvl + 1, prev, arr, dp);
+        //Take
+        int take=0;
+        if(last==-1 || nums[ind]>nums[last])
+            take=1+rec(ind+1,ind,nums,dp);
 
-        // Case 2: Take the current element (only if it forms an increasing subsequence)
-        int take = 0;
-        if (prev == -1 || arr[lvl] > arr[prev]) {
-            take = 1 + rec(lvl + 1, lvl, arr, dp); // Increment length by 1 when taking the element
-        }
+        //Nottake
+        int nttake=rec(ind+1,last,nums,dp);
 
-        // Store the result in dp and return the max of taking or not taking the element
-        return dp[lvl][prev+1] = max(take, notTake);
-    }  
+        return dp[ind][last+1]=max(take,nttake);
+    }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
         vector<vector<int>>dp(n+2,vector<int>(n+2,-1));
