69bfbdb4514cf97d6eeb93d695a0fb10911ab263
Vijeta Priya <67923889+VijetaPriya47@users.noreply.github.com>
2025-02-11 23:41:14 +0530

    Time: 2 ms (41.77%), Space: 15 MB (93.72%) - LeetHub

---
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& Arr) {
+           int n = Arr.size();
+    
+    // Initialize three arrays to track the maximum profit for buying and selling
+    vector<int> cur(2, 0);       // Current maximum profit
+    vector<int> front1(2, 0);    // Maximum profit one step ahead
+    vector<int> front2(2, 0);    // Maximum profit two steps ahead
+    
+    for (int ind = n - 1; ind >= 0; ind--) {
+        for (int buy = 0; buy <= 1; buy++) {
+            int profit;
+
+            if (buy == 0) { // We can buy the stock
+                profit = max(0 + front1[0], -Arr[ind] + front1[1]);
+            }
+
+            if (buy == 1) { // We can sell the stock
+                profit = max(0 + front1[1], Arr[ind] + front2[0]);
+            }
+            
+            cur[buy] = profit;
+        }
+        
+        // Update the 'front1' and 'front2' arrays with current values
+        front2 = front1;
+        front1 = cur;
+    }
+    
+    return cur[0]; // Return the maximum profit for buying.
+    }
+};
