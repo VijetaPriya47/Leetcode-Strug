beaab0ec5e6c492771ca91c722b82519f6e40978
Vijeta Priya <67923889+VijetaPriya47@users.noreply.github.com>
2025-02-14 15:45:39 +0530

    Time: 23 ms (69.87%), Space: 17.6 MB (92.15%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,32 +1,17 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
-        vector<int>v(amount+1);
-        for(int i=0;i<n;i++){
-            v[0] = 0;
-        }
-        for(int i=0;i<=amount;i++){
-            if(i % coins[0] == 0){
-                v[i] = i / coins[0];
-            }
-            else{
-                v[i] = 1e9;
-            }
-        }
-        for(int i=1;i<n;i++){
-            vector<int>curr(amount+1);
-            for(int j=0;j<=amount;j++){
-                int pick = 1e9;
-                if(coins[i] <= j){
-                    pick = 1+ curr[j-coins[i]];
+        vector<int> minCoins(amount + 1, amount + 1);
+        minCoins[0] = 0;
+
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.size(); j++) {
+                if (i - coins[j] >= 0) {
+                    minCoins[i] = min(minCoins[i], 1 + minCoins[i - coins[j]]);
                 }
-                int no_pick = v[j];
-                curr[j] = min(pick,no_pick);
             }
-            v = curr;
         }
-        if(v[amount] >= 1e9)return -1;
-        return v[amount];
+
+        return minCoins[amount] != amount + 1 ? minCoins[amount] : -1;        
     }
 };
