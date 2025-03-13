cd4a5a10d8910bd12e0a1a64f62b610626dd2402
Vijeta Priya <67923889+VijetaPriya47@users.noreply.github.com>
2025-03-14 02:18:58 +0530

    Time: 529 ms (47.29%), Space: 274.4 MB (80.99%) - LeetHub

--- 3789-maximize-subarrays-after-removing-one-conflicting-pair/3789-maximize-subarrays-after-removing-one-conflicting-pair.cpp
+++ 3789-maximize-subarrays-after-removing-one-conflicting-pair/3789-maximize-subarrays-after-removing-one-conflicting-pair.cpp
@@ -9,29 +9,29 @@
 using namespace std;
 
 typedef pair<int, int> PII;
-typedef long long LL;
+typedef long long ll;
 
 template <typename T> bool chkMax(T &x, T y) { return (y > x) ? x = y, 1 : 0; }
 template <typename T> bool chkMin(T &x, T y) { return (y < x) ? x = y, 1 : 0; }
 
 class Solution {
 public:
-    vector<PII> g[100005];
-    LL mx[200005];
+    vector<pair<int,int>>vec[100005];
+    ll mx[200005];
     
     long long maxSubarrays(int n, vector<vector<int>>& c) {
-        int m = c.size();
-        for (int i = 0; i < m; i++) {
-            int l = c[i][0], r = c[i][1];
-            if (l > r) swap(l, r);
-            //l++, r++;
-            g[r].pb(mp(l, i + 1));
+        int m=c.size();
+        for(int i=0;i<m;i++){
+            int l=c[i][0],r=c[i][1];
+            if(l>r)swap(l,r);
+            vec[r].pb(mp(l,i+1));
         }
-        LL s = 0;
-        LL m1 = 0, m2 = 0;
+
+        ll s=0;
+        ll m1=0, m2=0;
         int p1 = 0;
         for (int i = 1; i <= n; i++) {
-            for (PII o: g[i]) {
+            for (PII o: vec[i]) {
                 if (o.fi >= m1) {
                     m2 = m1;
                     m1 = o.fi;
@@ -47,7 +47,7 @@ public:
             }
         }
        // cout << s << endl;
-        LL ans = 0;
+        ll ans = 0;
         for (int i = 1; i <= m; i++)
             chkMax(ans, mx[i]);
         return ans + s;
