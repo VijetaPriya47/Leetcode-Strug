7be87b54d9b3524be795a2d4d5da1a2aa16b2168
Vijeta Priya <67923889+VijetaPriya47@users.noreply.github.com>
2025-03-14 00:26:20 +0530

    Time: 399 ms (9.78%), Space: 254.3 MB (5.04%) - LeetHub

--- 3759-choose-k-elements-with-maximum-sum/3759-choose-k-elements-with-maximum-sum.cpp
+++ 3759-choose-k-elements-with-maximum-sum/3759-choose-k-elements-with-maximum-sum.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
-    vector<long long> findMaxSum(vector<int>& nums1, vector<int>& nums2, int k) {
+    vector<long long> findMaxSum(vector<int>& n1, vector<int>& n2, int k) {
+        int n=n1.size();
+
+        // vector<pair<int,int>>vec(n,pair<int,int>());
         map<int,vector<int>>mp;
-        for(int i=0;i<nums1.size();i++){
-            mp[nums1[i]].push_back(i);
+        for(int i=0;i<n;i++){
+            mp[n1[i]].push_back(i);
         }
-        priority_queue<int,vector<int>,greater<int>>pq;
-        long long sum=0;
-        vector< long long>ans(nums1.size());
-        for(auto & i:mp){
-            int temp = sum;
-            for(auto  & j:i.second){
-                ans[j]=sum;
-            }
-            for(auto & j:i.second){
-                sum+=nums2[j];
-                pq.push(nums2[j]);
-                if(pq.size()>k){
-                    sum-=pq.top();
+
+        priority_queue<int, vector<int>,greater<int>>pq;
+        vector<long long>ans(n,0);
+        long long result=0;
+        for(auto [x,y]:mp){
+
+            while(pq.size()>k){
+                    result-=pq.top();
                     pq.pop();
-                }
             }
+            long long sum=0;
+            for(auto xx:y){
+                ans[xx]+=result;
+                pq.push(n2[xx]);
+                sum+=n2[xx];
+            }
+            result+=sum;
         }
         return ans;
-        
     }
 };
